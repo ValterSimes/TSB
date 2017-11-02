cb7f6ad7257ee970f8a8c1825b2b58810be3b0e0
Lupo28 <31636437+Lupo28@users.noreply.github.com>
2017-11-02 16:48:45 -0300

    Clase Entry hecha

--- TSBHashtable/src/main/java/Datos/TSB_OAHashtable.java
+++ TSBHashtable/src/main/java/Datos/TSB_OAHashtable.java
@@ -1,14 +1,100 @@
 package Datos;
 
 import java.io.Serializable;
+import java.util.AbstractSet;
+import java.util.AbstractCollection;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.Set;
 
 public class TSB_OAHashtable<K,V> implements Map<K,V>, Cloneable, Serializable
 {
 
+    public TSB_OAHashtable
+            {
+    
+            }
 
+            /*
+     * Clase interna que representa los pares de objetos que se almacenan en la
+     * tabla hash: son instancias de esta clase las que realmente se guardan en 
+     * en cada una de las listas del arreglo table que se usa como soporte de 
+     * la tabla. Lanzará una IllegalArgumentException si alguno de los dos 
+     * parámetros es null.
+     */
+    private class Entry<K, V> implements Map.Entry<K, V>
+    {
+        private K key;
+        private V value;
+        
+        public Entry(K key, V value) 
+        {
+            if(key == null || value == null)
+            {
+                throw new IllegalArgumentException("Entry(): parámetro null...");
+            }
+            this.key = key;
+            this.value = value;
+        }
+        
+        @Override
+        public K getKey() 
+        {
+            return key;
+        }
+
+        @Override
+        public V getValue() 
+        {
+            return value;
+        }
+
+        @Override
+        public V setValue(V value) 
+        {
+            if(value == null) 
+            {
+                throw new IllegalArgumentException("setValue(): parámetro null...");
+            }
+                
+            V old = this.value;
+            this.value = value;
+            return old;
+        }
+       
+        @Override
+        public int hashCode() 
+        {
+            int hash = 7;
+            hash = 61 * hash + Objects.hashCode(this.key);
+            hash = 61 * hash + Objects.hashCode(this.value);            
+            return hash;
+        }
+
+        @Override
+        public boolean equals(Object obj) 
+        {
+            if (this == obj) { return true; }
+            if (obj == null) { return false; }
+            if (this.getClass() != obj.getClass()) { return false; }
+            
+            final Entry other = (Entry) obj;
+            if (!Objects.equals(this.key, other.key)) { return false; }
+            if (!Objects.equals(this.value, other.value)) { return false; }            
+            return true;
+        }       
+        
+        @Override
+        public String toString()
+        {
+            return "(" + key.toString() + ", " + value.toString() + ")";
+        }
+    }
     @Override
     public int size() {
         return 0;
@@ -36,6 +122,7 @@ public class TSB_OAHashtable<K,V> implements Map<K,V>, Cloneable, Serializable
 
     @Override
     public V put(K key, V value) {
+        
         return null;
     }
 
